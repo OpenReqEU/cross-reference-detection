267516870a405e62c967bf12c146035b4f94a57f
Quim <joaquim.motger@est.fib.upc.edu>
2019-03-11 18:08:13 +0100

    update README + add meaningful tests

--- README.md
+++ README.md
@@ -1,19 +1,59 @@
-# Cross-Reference Detection API
+# Cross Reference Detection
 
-#### Web Service Swagger Documentation:
-/upc/cross-reference-detection/swagger-ui.html
+_This service was created as a result of the OpenReq project funded by the European Union Horizon 2020 Research and Innovation programme under grant agreement No 732463._
 
+## Introduction
 
-#### Web Services:
-/upc/cross-reference-detection/file
+The **cross-reference detection** tool is a Requirement Engineering (RE) tool used to identify cross-references from requirements. These requirements can be in a database, or in an HTML document. Cross-references are dependencies explicitly stated, either internal (between requirements of the same project), or external (between a requirement and an external source).
 
-	
-/upc/cross-reference-detection/file/{n}/{m}
+## Technical description
 
+Next sections provide a general overview of the technical details of the dependency detection tool.
 
-/upc/cross-reference-detection/json/{projectId}
+### Functionalities
 
+#### 1. Cross-reference detection: HTML file
 
-/upc/cross-reference-detection/json/{projectId}/{n}/{m}
+Uploads one document in HTML format to the server, extracts the cross-references of all the requirements in the document and finally removes the uploaded file.
 
+- **Indexed detection** -> the component allows to extract the cross-references of the requirements between N-M indexes in the document.
 
+#### 2. Cross-reference detection: JSON project
+
+Extracts the cross-references of all bugs and requirements of a project stored in an input JSON.
+
+- **Indexed detection** -> the component allows to extract the cross-references between N-M indexes of all bugs and requirements of the input JSON.
+
+### Used technologies
+
+* Swagger (&rarr; [https://swagger.io/](https://swagger.io/) )
+* Maven (&rarr; [https://maven.apache.org/](https://maven.apache.org/) )
+* jsoup: Java HTML Parser (&rarr; [https://jsoup.org/](https://jsoup.org/) )
+
+### How to install
+
+The project does not require the addition of external dependencies or additional configuration. It can be built by running:
+
+```
+mvn clean install package
+```
+
+This generates a .jar file.
+
+### How to use it
+
+You can take a look at the Swagger documentation [here](https://api.openreq.eu/cross-reference-detection/swagger-ui.html), which includes specific, technical details of the REST API to communicate to the service.
+
+### Notes for developers
+
+### Sources
+
+* "Jsoup Java HTML Parser, with Best of DOM, CSS, and Jquery." Jsoup Java HTML Parser, with Best of DOM, CSS, and Jquery. Accessed March 11, 2019. https://jsoup.org/.
+
+## How to contribute
+
+See OpenReq project contribution [guidelines](https://github.com/OpenReqEU/OpenReq/blob/master/CONTRIBUTING.md)
+
+## License
+
+Free use of this software is granted under the terms of the [EPL version 2 (EPL2.0)](https://www.eclipse.org/legal/epl-2.0/)
--- src/test/java/com/essi/Dependency/AppTest.java
+++ src/test/java/com/essi/Dependency/AppTest.java
@@ -1,34 +1,70 @@
 package com.essi.Dependency;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName
-     *            name of the test case
-     */
-    public AppTest(String testName) {
-	super(testName);
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private String path = "./testing/integration/";
+
+    @Test
+    public void crossReferenceJSONProject() throws Exception {
+        String projectId = "QTBUG";
+        String response = this.mockMvc.perform(
+                post("/upc/cross-reference-detection/json/" + projectId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(read_file(path+"sample-1.json")))
+                .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        JSONObject result = new JSONObject(response);
+        Assert.assertEquals(result.getJSONArray("dependencies").length(), 1);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-	return new TestSuite(AppTest.class);
+    private String read_file(String path) throws Exception {
+        String result = "";
+        String line = "";
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                result = result.concat(line);
+            }
+            bufferedReader.close();
+            JSONObject aux = new JSONObject(result);
+            return aux.toString();
+        } finally {
+            if (fileReader != null) fileReader.close();
+            if (bufferedReader != null) bufferedReader.close();
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-	assertTrue(true);
-    }
+
 }
