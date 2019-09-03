119c49fd6666accfb4244522e2c200a3a6d4128b
Quim <joaquim.motger@est.fib.upc.edu>
2019-09-03 13:40:00 +0200

    add html file tests

--- src/test/java/com/essi/Dependency/AppTest.java
+++ src/test/java/com/essi/Dependency/AppTest.java
@@ -11,8 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -45,6 +49,67 @@ public class AppTest {
                 .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
         JSONObject result = new JSONObject(response);
         Assert.assertEquals(1, result.getJSONArray("dependencies").length());
+    }
+
+    @Test
+    public void crossReferenceJSONProjectNM() throws Exception {
+        String projectId = "QTBUG";
+        String response = this.mockMvc.perform(
+                post("/upc/cross-reference-detection/json/" + projectId + "/1/2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(read_file(path+"sample-1.json")))
+                .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        JSONObject result = new JSONObject(response);
+        Assert.assertEquals(1, result.getJSONArray("dependencies").length());
+    }
+
+    @Test
+    public void crossReferenceHTMLProject() throws Exception {
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file","sample-2.html",
+                "text/html", read_html_file(path+"sample-2.html").getBytes());
+
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.fileUpload("/upc/cross-reference-detection/file")
+                        .file(mockMultipartFile);
+
+        String response = this.mockMvc.perform(builder).andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print()).andReturn().getResponse().getContentAsString();;
+        JSONObject result = new JSONObject(response);
+        Assert.assertEquals(1, result.getJSONArray("dependencies").length());
+    }
+
+    @Test
+    public void crossReferenceHTMLProjectNM() throws Exception {
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file","sample-2.html",
+                "text/html", read_html_file(path+"sample-2.html").getBytes());
+
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.fileUpload("/upc/cross-reference-detection/file/1/2")
+                        .file(mockMultipartFile);
+
+        String response = this.mockMvc.perform(builder).andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print()).andReturn().getResponse().getContentAsString();;
+        JSONObject result = new JSONObject(response);
+        Assert.assertEquals(0, result.getJSONArray("dependencies").length());
+    }
+
+    private String read_html_file(String path) throws Exception {
+        String result = "";
+        String line = "";
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                result = result.concat(line);
+            }
+            bufferedReader.close();
+            return result;
+        } finally {
+            if (fileReader != null) fileReader.close();
+            if (bufferedReader != null) bufferedReader.close();
+        }
     }
 
     private String read_file(String path) throws Exception {
